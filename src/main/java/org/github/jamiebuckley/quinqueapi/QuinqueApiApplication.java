52a5cfc6fe82e9d19680c05415e63787ab7cdfda
Jamie <jbuckley760@gmail.com>
2023-02-17 19:21:39 +0000

    Initial commit

---
+++ src/main/java/org/github/jamiebuckley/quinqueapi/QuinqueApiApplication.java
@@ -0,0 +1,13 @@
+package org.github.jamiebuckley.quinqueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuinqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuinqueApiApplication.class, args);
+	}
+
+}
